d438be39cede5de09f0f1978c8fcec424ad8b682
dongcc0219 <dongcc0219@163.com>
2020-05-20 17:19:01 +0800

    全都是错是。没错

---
+++ src/main/java/com/dongcc/mychat/config/WebSocketConfig.java
@@ -0,0 +1,14 @@
+package com.dongcc.mychat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Controller
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+}
